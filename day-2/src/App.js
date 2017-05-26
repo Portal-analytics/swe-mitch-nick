23af655b9c49ed580afc713f0c3fe10d03fa7797
Mitch Gillin <mtg4ra@virginia.edu>
2017-05-26 10:00:23 -0400

    Added button and bar

--- day-2/src/App.js
+++ day-2/src/App.js
@@ -1,40 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+
+var quotes = ["GET FUCKING MONEY", "BE FUCKING NICE TO OTHERS", "DONT BE FUCKING EVIL"];
 
 
-var quotes=["GET FUCKING MONEY","BE FUCKING NICE TO OTHERS","DONT BE FUCKING EVIL"];
+const AppBarExampleIcon = () => (
+  <AppBar
+    title='Great Fucking Startup Advice'
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />
+);
+
+const FlatButtonExampleSimple = () => (
+  <div>
+    <FlatButton label="Next Quote" />
+    <br />
+    <br />
+  </div>
+);
+
+
+
 
 class App extends Component {
-  state=
+  state =
   {
     clicks: 0
   }
 
-nextQuote = () => {
 
-  this.setState({clicks:this.state.clicks+1});
+
+
+  nextQuote = () => {
+
+    this.setState({ clicks: this.state.clicks + 1 });
   }
 
 
-  render(){ 
+  render() {
     return (
+      
       <div className='App'>
+        
         <div className='App-header'>
-          <h2>
-          Great Fucking
-          <br></br>
-          Startup Advice 
-          </h2>
+          <MuiThemeProvider>
+            <AppBarExampleIcon >
+            </AppBarExampleIcon>
+          </MuiThemeProvider>
         </div>
-        <button 
-            className="button"
-            onClick={(e) => {this.nextQuote(this)}}>    
-            Click4Quotes!
-        </button>
+        <MuiThemeProvider>
+          <FlatButtonExampleSimple>
+          </FlatButtonExampleSimple>
+        </MuiThemeProvider>
+
         <h1 className="App-quotes" >
           {quotes[this.state.clicks]}
         </h1>
+
       </div>
+
+
     );
   }
 }
