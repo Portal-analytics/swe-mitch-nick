c8fc1597e276b527aa8ba488b52ecc4cf0a1ed52
Mitch Gillin <mtg4ra@virginia.edu>
2017-05-30 08:57:29 -0400

    updating all of the files

--- day-2/src/App.js
+++ day-2/src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
 
-var quotes = ["GET FUCKING MONEY", "BE FUCKING NICE TO OTHERS", "DONT BE FUCKING EVIL"];
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import Person from 'react-icons/lib/md/person-pin'
+import Info from 'react-icons/lib/md/info'
+import Language from 'react-icons/lib/md/language'
+
+
+
+injectTapEventPlugin();
+
+
+var quotes = ["GET FUCKING MONEY", "BE FUCKING NICE TO OTHERS", 
+"DONT BE FUCKING EVIL", "STOP FUCKING AROUND AND GET BACK TO WORK", 
+"STOP WATCHING FUCKING NETFLIX ALL DAY YOU FUCKER",
+"JUST HIRE A FUCKING LAWYER ALREADY",
+"CUT THE FUCKING DEAD WEIGHT",
+"BE A FUCKING LEARNING MACHINE "];
 
 
 const AppBarExampleIcon = () => (
@@ -18,8 +35,12 @@ const AppBarExampleIcon = () => (
 const FlatButtonExampleSimple = () => (
   <div>
     <FlatButton label="Next Quote" />
-    <br />
-    <br />
+  </div>
+);
+
+const FlatButtonExampleRand = () => (
+  <div>
+    <FlatButton label="Random Quote" />
   </div>
 );
 
@@ -29,37 +50,80 @@ const FlatButtonExampleSimple = () => (
 class App extends Component {
   state =
   {
-    clicks: 0
+    clicks: 0,
+    totalClicks: 0
   }
-
-
 
 
   nextQuote = () => {
 
-    this.setState({ clicks: this.state.clicks + 1 });
+    if (this.state.clicks === quotes.length - 1) {
+      this.setState({ clicks: 0 });
+      this.setState({ totalClicks: this.state.totalClicks + 1 })
+    }
+    else {
+      this.setState({ clicks: this.state.clicks + 1 });
+      this.setState({ totalClicks: this.state.totalClicks + 1 })
+    }
+  }
+
+  nextRand = () => {
+    var randNum = Math.floor((Math.random() * quotes.length));
+    if(randNum==this.state.clicks)
+    {
+      this.nextRand();
+    }
+    else{
+     this.setState({ clicks: randNum });
+      this.setState({ totalClicks: this.state.totalClicks + 1 })
+    }
+
   }
 
 
   render() {
     return (
-      
+
       <div className='App'>
-        
+
         <div className='App-header'>
           <MuiThemeProvider>
             <AppBarExampleIcon >
             </AppBarExampleIcon>
           </MuiThemeProvider>
         </div>
+
+
         <MuiThemeProvider>
-          <FlatButtonExampleSimple>
-          </FlatButtonExampleSimple>
+          <div onClick={this.nextQuote}  >
+            <FlatButtonExampleSimple />
+          </div>
+
         </MuiThemeProvider>
+
+        <MuiThemeProvider>
+
+          <div onClick={this.nextRand} >
+            <FlatButtonExampleRand />
+          </div>
+        </MuiThemeProvider>
+
+
+
 
         <h1 className="App-quotes" >
           {quotes[this.state.clicks]}
         </h1>
+
+        <h2 className="Click-Counter">
+          Number of Clicks: {this.state.totalClicks}
+        </h2>
+
+        <div className="social-media">
+        <ul>
+         <li><Person/></li> <li><Info/></li> <li><Language/></li>
+        </ul>
+        </div>
 
       </div>
 
