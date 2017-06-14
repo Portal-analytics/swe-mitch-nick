e2489a37693da3487c75b8dcd9ccbb29da4fc985
Nick Onley <nick.onley@aol.com>
2017-06-14 11:06:57 -0400

    Functioning text input and message display

--- NickMorningChallenge8/App.js
+++ NickMorningChallenge8/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, TextInput } from "react-native";
 import { NativeRouter, Route, Link } from "react-router-native";
 import NavBar, { NavButton, NavButtonText, NavTitle } from "react-native-nav";
 import SideMenu from "react-native-side-menu";
@@ -8,6 +8,36 @@ import Settings from "./settings";
 import Profile from "./profile";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+      messages: []
+    };
+  }
+
+  //TextBox Funtions
+  handleTextEditing = text => {
+    this.setState({
+      ...this.state,
+      message: text
+    });
+  };
+
+  handleTextSubmit = () => {
+    let newMessageList = this.state.messages;
+    let newMessage = {
+      message: this.state.message + "  " + new Date().toTimeString().slice(0, 5)
+    };
+    newMessageList.unshift(newMessage);
+
+    this.setState({
+      ...this.state,
+      message: "",
+      messages: newMessageList
+    });
+  };
+
   render() {
     return (
       <View>
@@ -37,6 +67,19 @@ export default class App extends React.Component {
             </View>
           </View>
         </NativeRouter>
+        <View>
+          <TextInput
+            style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+            onChangeText={text => this.handleTextEditing(text)}
+            onSubmitEditing={() => this.handleTextSubmit()}
+            returnKeyType="send"
+            clearButtonMode="always"
+            value={this.state.message}
+          />
+        </View>
+        <View>
+          {this.state.messages.map(text => <Text>{text.message}</Text>)}
+        </View>
       </View>
     );
   }
@@ -52,7 +95,3 @@ const styles = StyleSheet.create({
     alignItems: "flex-start"
   }
 });
-
-/*<Link to="/home">
-  <Text>{}</Text>
-</Link>;*/
